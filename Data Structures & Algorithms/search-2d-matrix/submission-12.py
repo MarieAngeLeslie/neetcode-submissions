493c446c5b451e53902d4022f234b572f6e27132
MarieAngeLeslie <42603506+MarieAngeLeslie@users.noreply.github.com>
2026-08-13 06:24:54 +0000

    Add: search-2d-matrix - submission-12

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-12.py
@@ -0,0 +1,41 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        matrix_lower_bound = 0
+        matrix_upper_bound = len(matrix) - 1
+
+        while matrix_lower_bound <= matrix_upper_bound : 
+            middle_list = (matrix_lower_bound + matrix_upper_bound)//2
+
+            if matrix[middle_list][0] <= target and matrix[middle_list][-1] >= target :
+                return self.__searchElement( matrix[middle_list] , target)
+
+            if matrix[middle_list][0] > target : 
+                matrix_upper_bound = middle_list - 1
+
+            if matrix[middle_list][-1] < target :
+                matrix_lower_bound = middle_list + 1
+
+        return False
+
+
+    def __searchElement(self, tinyMatrix: List[int], target: int) -> bool:
+        lower_bound = 0
+        upper_bound = len(tinyMatrix) - 1
+
+        while lower_bound <= upper_bound : 
+            middle = (lower_bound + upper_bound)//2
+
+            if tinyMatrix[middle] > target : 
+                upper_bound = middle - 1
+                continue
+
+            if tinyMatrix[middle] < target :
+                lower_bound = middle + 1
+                continue
+            
+            return True
+
+        return False
+
+        
